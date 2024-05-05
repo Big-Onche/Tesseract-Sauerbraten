8402f7a6874ac8ef4cb68d6f1ec4c34b2335f1d3
CubeConflict <vod-k@live.be>
2024-05-05 21:30:40 +0200

    fix | first person playermodel showing when spectator mode

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -1187,7 +1187,7 @@ void renderclient(dynent *d, const char *mdlname, modelattach *attachments, int 
     else flags |= MDL_CULL_DIST;
     if(d->state==CS_LAGGED) fade = min(fade, 0.3f);
     if(drawtex == DRAWTEX_MODELPREVIEW) flags &= ~(MDL_FULLBRIGHT | MDL_CULL_VFC | MDL_CULL_OCCLUDED | MDL_CULL_QUERY | MDL_CULL_DIST);
-    bool onlyshadow = (d==game::hudplayer() && game::hudplayer()->state==CS_ALIVE) || editmode;
+    bool onlyshadow = (d==game::hudplayer() && game::hudplayer()->state!=CS_DEAD);
     rendermodel(mdlname, anim, o, yaw, 0, onlyshadow ? pitch/3.f : pitch, onlyshadow && !thirdperson ? MDL_ONLYSHADOW : flags, d, attachments, basetime, 0, fade);
 }
 
