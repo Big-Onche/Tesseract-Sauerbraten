95e026537b4df7f8edbd66fee3d40a0a7207b799
Salatiel <9287152+SalatielSauer@users.noreply.github.com>
2024-05-12 17:23:16 -0300

    fix | playermodel pitch & yaw
    removes the yaw increment from the renderclient and specifies it in the models' cfg.

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -1138,7 +1138,7 @@ VAR(testpitch, -90, 0, 90);
 void renderclient(dynent *d, const char *mdlname, modelattach *attachments, int hold, int attack, int attackdelay, int lastaction, int lastpain, float fade, bool ragdoll, bool onlyshadow)
 {
     int anim = hold ? hold : ANIM_IDLE|ANIM_LOOP;
-    float yaw = testanims && d==player ? 0 : d->yaw+90,
+    float yaw = testanims && d==player ? 0 : d->yaw,
           pitch = testpitch && d==player ? testpitch : d->pitch;
     vec o = d->feetpos();
     int basetime = 0;
@@ -1188,7 +1188,7 @@ void renderclient(dynent *d, const char *mdlname, modelattach *attachments, int 
     else flags |= MDL_CULL_DIST;
     if(d->state==CS_LAGGED) fade = min(fade, 0.3f);
     if(drawtex == DRAWTEX_MODELPREVIEW) flags &= ~(MDL_FULLBRIGHT | MDL_CULL_VFC | MDL_CULL_OCCLUDED | MDL_CULL_QUERY | MDL_CULL_DIST);
-    rendermodel(mdlname, anim, o, yaw, 0, onlyshadow ? pitch/3.f : pitch, onlyshadow && !thirdperson ? MDL_ONLYSHADOW : flags, d, attachments, basetime, 0, fade);
+    rendermodel(mdlname, anim, o, yaw, pitch, 0, onlyshadow && !thirdperson ? MDL_ONLYSHADOW : flags, d, attachments, basetime, 0, fade);
 }
 
 void setbbfrommodel(dynent *d, const char *mdl)
